7dfe98cbb2e7bfc7e20d33e535618c691f56e8c2
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-06-30 23:48:56 +0200

    Eslint, Jest and Sass installed

--- README.md
+++
@@ -1,1 +0,0 @@
-# BeMobile-TechnicalTest-priv
---
+++ techtest-bmb/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'comma-dangle': [2, 'never'],
+    'linebreak-style': 0,
+    'global-require': 0,
+    'eslint linebreak-style': [0, 'error', 'windows'],
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }]
+  },
+  extends: [
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript"
+  ]
+};
