f0ba9ca7ce865ee43e96172e33f707977d39bd79
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-07-01 18:35:54 +0200

    Store created & combine reducers

---
+++ techtest-bmb/src/redux/reducers/index.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import moviesReducer from './moviesReducer';
+
+
+const rootReducer = combineReducers({
+   movies: moviesReducer,
+});
+
+export default rootReducer;
