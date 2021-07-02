539900f598790d213161f88408ea397edbe8acc6
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-07-02 17:24:46 +0200

    Create interface for tvShows Api response

---
+++ techtest-bmb/src/common/TvShowsInterfaces.ts
@@ -0,0 +1,33 @@
+export interface ApiTvShowResponse {
+    page: number;
+    results: TvShow[];
+    total_pages: number;
+    total_results: number;
+}
+
+export interface TvShow {
+    first_air_date:  Date;
+    name: string;
+    overview: string;
+    original_name: string;
+    origin_country: string[];
+    backdrop_path: string;
+    genre_ids: number[];
+    original_language: string;
+    vote_count: number;
+    poster_path: string;
+    vote_average: number;
+    id: number;
+    popularity: number;
+    media_type: MediaType;
+}
+
+export enum MediaType {
+    Tv = "tv",
+}
+
+export type TvShowActions = {
+    type: string;
+    tvShows?:TvShow[];
+    selectedTvShow?:TvShow;
+}
