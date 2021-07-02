90437185b699d44c0dcdbef4664a391372c415fc
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-07-02 02:03:43 +0200

    Implement link to movieDetail

--- techtest-bmb/src/components/MovieDetails.jsx
+++ techtest-bmb/src/components/MovieDetails.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export const MovieDetails = () => {
-    return (
-        <div>
-            <h1>Single movie detail page</h1>
-        </div>
-    )
-}
+  const { movieId } = useParams();
+
+  return (
+    <div>
+      <h1>
+        Single movie detail page
+        {' '}
+        {movieId}
+      </h1>
+    </div>
+  );
+};
--- techtest-bmb/src/components/MoviesList.tsx
+++ techtest-bmb/src/components/MoviesList.tsx
@@ -1,39 +1,71 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from 'react';
-import { connect, useDispatch, useSelector } from 'react-redux';
-import printImg from '../helpers/printImg';
-import printMovies from '../helpers/printMovies';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Movie } from '../common/interfaces';
+// import printImg from '../helpers/printImg';
+// import printMovies from '../helpers/printMovies';
 // import { Movie } from '../common/interfaces';
 import { loadMovies } from '../redux/actions/actionCreators';
 import { AppState } from '../redux/reducers';
 
-
-const MoviesList = () => {
-  const movies = useSelector((store: AppState) => store.movies)
+const MoviesList = ():JSX.Element => {
+  const [selectedMovie, setSelectedMovie] = useState<number>();
+  const movies = useSelector((store: AppState) => store.movies);
   const dispatch = useDispatch();
 
   useEffect(() => {
-      dispatch(loadMovies());
-    }, []);
-      
-    return (
-        <div>
-            <h1>MovieList</h1>
-            <ul>
-              {printMovies(movies)}
-              {printImg(movies)}
-            </ul>
-        </div>
-    )
-};
+    dispatch(loadMovies());
+  }, []);
 
+  const handleSelectedHero = (id:number) => {
+    setSelectedMovie(id);
+    // dispatch(selectedMovie())
+  };
+
+  //   function printMovies(array:Movie[]): JSX.Element[] { ///TODO
+  //     return (array.map( ({id, title}:Movie) => {
+  //        return (
+  //             <button key={id} onClick={setSelectedMovie(id)}>
+  //                  <li >
+  //                    {title}
+  //                  </li>
+  //             </button>
+  //        );
+  //      })
+  //    )
+  //  };
+
+  return (
+    <div>
+      <h1>MovieList</h1>
+      <ul>
+        {movies.map(({ id, title, poster_path }:Movie) => (
+          <button key={id} type="button" onClick={() => { handleSelectedHero(id); }}>
+            <Link to={`/details/${id}`}>
+              <li>
+                {title}
+                <img src={`https://image.tmdb.org/t/p/original/${poster_path}`} alt="movie poster" height="10%" width="10%" />
+
+              </li>
+            </Link>
+          </button>
+        ))}
+
+        {/* {printImg(movies)} */}
+        {/* {printMovies(movies)} */}
+        {/* {printMovies(movies, setSelectedMovie)} */}
+      </ul>
+    </div>
+  );
+};
 
 // function mapStateToProps(store:any) {
 //     return {
 //       movies: store.movies,
 //     };
 //   };
-  
+
 export default MoviesList;
 // export default connect(mapStateToProps)(MoviesList);
