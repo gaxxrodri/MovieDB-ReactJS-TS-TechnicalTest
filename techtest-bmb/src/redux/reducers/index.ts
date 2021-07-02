2155b85fc8107fb76df8b2c8b49258a1f044d5a5
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-07-02 17:30:14 +0200

    Add tvShows to rootReducer element

--- techtest-bmb/src/redux/reducers/index.ts
+++ techtest-bmb/src/redux/reducers/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import moviesReducer from './moviesReducer';
 import selectedMovieReducer from './selectedMovieReducer';
+import tvShowsReducer from './tvShowsReducer';
 
 const rootReducer = combineReducers({
   movies: moviesReducer,
   selectedMovie: selectedMovieReducer,
+  tvShows: tvShowsReducer,
 });
 
 export type AppState = ReturnType<typeof rootReducer>
