9c8ef77cc0d7461891ed53118752bd9bf58b06b5
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-07-02 17:57:16 +0200

    Add getTvShowById function in actionCreator

--- techtest-bmb/src/redux/actions/actionCreators.ts
+++ techtest-bmb/src/redux/actions/actionCreators.ts
@@ -2,6 +2,7 @@ import { Dispatch } from 'redux';
 import { Movie } from '../../common/MovieInterfaces';
 import api from '../../services/api';
 import actionTypes from './actionTypes';
+import { TvShow } from '../../common/TvShowsInterfaces';
 
 export const loadMovies = () => async (dispatch: Dispatch) => {
   const movies = await api.movies.fetch();
@@ -17,9 +18,9 @@ export const loadMovies = () => async (dispatch: Dispatch) => {
   }
 };
 
-export const getMovieById = (SelectedMovieId:number, movies:Movie[]) => (dispatch:Dispatch) => {
+export const getMovieById = (movieId:number, movies:Movie[]) => (dispatch:Dispatch) => {
   
-  const selectedMovie = movies.find((movie:Movie)=> movie.id === SelectedMovieId)
+  const selectedMovie = movies.find((movie:Movie)=> movie.id === movieId)
   try {
     dispatch({
       type: actionTypes.LOAD_SELECTED_MOVIE,
@@ -39,6 +40,23 @@ export const loadTvShows = () => async (dispatch: Dispatch) => {
     dispatch({
       type: actionTypes.LOAD_TVSHOWS,
       tvShows,
+    });
+  } catch (error) {
+    dispatch({
+      type: actionTypes.LOAD_TVSHOWS_ERROR,
+    });
+  }
+};
+
+export const getTvShowById = (tvShowId:number, tvShows:TvShow[]) => (dispatch:Dispatch) => {
+  
+  const selectedTvShow = tvShows.find((tvShow:TvShow)=> tvShow.id === tvShowId)
+  console.log(tvShows)
+
+  try {
+    dispatch({
+      type: actionTypes.LOAD_SELECTED_TVSHOW,
+      selectedTvShow,
     });
   } catch (error) {
     dispatch({
