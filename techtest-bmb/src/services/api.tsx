44705df1eb240b8809e076f0fd47e7c8a19dbda0
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-07-02 17:26:33 +0200

    Add tvShows api call to api object

--- techtest-bmb/src/services/api.tsx
+++ techtest-bmb/src/services/api.tsx
@@ -1,17 +1,33 @@
 import axios from 'axios';
-import { Api, Movie } from '../common/interfaces';
+import { Api } from '../common/ApiInterface';
+import { Movie } from '../common/MovieInterfaces';
+import { TvShow } from '../common/TvShowsInterfaces';
 
-const URL = process.env.REACT_APP_URL;
+const URL_MOVIES = process.env.REACT_APP_URL_MOVIES;
+const URL_TVSHOWS = process.env.REACT_APP_URL_TVSHOWS;
 const API = process.env.REACT_APP_KEY;
-const url_endpoint = `${URL}?api_key=${API}`;
 
 export const api:Api = {
   movies: {
     async fetch() {
+      const url_movies_endpoint = `${URL_MOVIES}?api_key=${API}`;
       try {
-        const { data } = await axios.get<Movie[] | any>(url_endpoint); // TODO
+        const { data } = await axios.get<Movie[] | any>(url_movies_endpoint); // TODO
         const movies = data.results;
         return movies;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  },
+  tvShows:{
+    async fetch(){
+      const url_tvShows_endpoint = `${URL_TVSHOWS}?api_key=${API}`;
+      try {
+        const { data } = await axios.get<TvShow[] | any>(url_tvShows_endpoint); // TODO
+        
+        const tvShows = data.results;
+        return tvShows;
       } catch (error) {
         console.error(error);
       }
