7c2fe58ccd456c6c2e24f9addc73742c20c17c91
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-07-02 17:28:57 +0200

    Create reducer for tvShows

---
+++ techtest-bmb/src/redux/reducers/tvShowsReducer.ts
@@ -0,0 +1,19 @@
+import { TvShow, TvShowActions } from '../../common/TvShowsInterfaces';
+import actionTypes from '../actions/actionTypes';
+
+function tvShowsReducer(tvShows = [], action:TvShowActions) {
+  let updateTvShows:TvShow[] | undefined = [...tvShows];
+
+  switch (action.type) {
+    case actionTypes.LOAD_TVSHOWS:
+        updateTvShows = action.tvShows;
+      break;
+    case actionTypes.LOAD_TVSHOWS_ERROR:
+      break;
+    default:
+      break;
+  }
+  return updateTvShows;
+}
+
+export default tvShowsReducer;
