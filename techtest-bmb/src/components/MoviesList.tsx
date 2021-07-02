5b5ead73509b1cb9f4e50f343288d802274664c7
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-07-02 16:14:55 +0200

    Refactor movieList component

--- techtest-bmb/src/components/MoviesList.tsx
+++ techtest-bmb/src/components/MoviesList.tsx
@@ -1,17 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Movie } from '../common/interfaces';
-// import printImg from '../helpers/printImg';
-// import printMovies from '../helpers/printMovies';
-// import { Movie } from '../common/interfaces';
 import { loadMovies } from '../redux/actions/actionCreators';
 import { AppState } from '../redux/reducers';
 
 const MoviesList = ():JSX.Element => {
-  const [selectedMovie, setSelectedMovie] = useState<number>();
   const movies = useSelector((store: AppState) => store.movies);
   const dispatch = useDispatch();
 
@@ -19,30 +15,12 @@ const MoviesList = ():JSX.Element => {
     dispatch(loadMovies());
   }, []);
 
-  const handleSelectedHero = (id:number) => {
-    setSelectedMovie(id);
-    // dispatch(selectedMovie())
-  };
-
-  //   function printMovies(array:Movie[]): JSX.Element[] { ///TODO
-  //     return (array.map( ({id, title}:Movie) => {
-  //        return (
-  //             <button key={id} onClick={setSelectedMovie(id)}>
-  //                  <li >
-  //                    {title}
-  //                  </li>
-  //             </button>
-  //        );
-  //      })
-  //    )
-  //  };
-
   return (
     <div>
       <h1>MovieList</h1>
       <ul>
         {movies.map(({ id, title, poster_path }:Movie) => (
-          <button key={id} type="button" onClick={() => { handleSelectedHero(id); }}>
+          <button key={id} type="button">
             <Link to={`/details/${id}`}>
               <li>
                 {title}
@@ -61,11 +39,4 @@ const MoviesList = ():JSX.Element => {
   );
 };
 
-// function mapStateToProps(store:any) {
-//     return {
-//       movies: store.movies,
-//     };
-//   };
-
 export default MoviesList;
-// export default connect(mapStateToProps)(MoviesList);
