241c4e3ebdc1c6496179464da0b95204951329c6
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-07-04 03:04:44 +0200

    Rename folders

--- techtest-bmb/src/App.tsx
+++ techtest-bmb/src/App.tsx
@@ -1,31 +1,39 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
 import {
   BrowserRouter,
   Route,
   Switch
 } from 'react-router-dom';
 import './App.css';
-import Header from './components/header/Header';
+import NavBar from './components/navBar/NavBar';
 import { MovieDetails } from './components/MovieDetails';
-import MoviesList from './components/MoviesList';
+import MoviesList from './components/moviesList/MoviesList';
 import NotFound from './components/NotFound';
 import { TvShowDetail } from './components/TvShowDetail';
 import TvShowsList from './components/TvShowsList';
+import store from './redux/store';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <main className="main-container">
-        <Header/>
-        <Switch>
-          <Route path="/" exact component={MoviesList} />
-          <Route path="/movie/details/:movieId" exact component={MovieDetails} />
-          <Route path="/tv" exact component={TvShowsList} />
-          <Route path="/tv/details/:tvShowId" exact component={TvShowDetail} />
-          <Route component={NotFound} />
-        </Switch>
-      </main>
-    </BrowserRouter>
+    <div className="App">
+      <Provider store={store({})}>
+        <BrowserRouter>
+          <main className="main-container">
+            <NavBar/>
+            <Switch>
+              <Route path="/" exact component={MoviesList} />
+              <Route path="/movie/details/:movieId" exact component={MovieDetails} />
+              <Route path="/tv" exact component={TvShowsList} />
+              <Route path="/tv/details/:tvShowId" exact component={TvShowDetail} />
+              <Route component={NotFound} />
+            </Switch>
+          </main>
+        </BrowserRouter>
+      </Provider>
+    </div>
   );
 }
 
