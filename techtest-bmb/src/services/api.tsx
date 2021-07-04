30251ba6f84e9d511025d4ea1d2244267443bff5
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-07-04 13:56:32 +0200

    Change endpoints format

--- techtest-bmb/src/services/api.tsx
+++ techtest-bmb/src/services/api.tsx
@@ -5,17 +5,21 @@ import { ApiSimilarTitlesResponse } from '../common/interfaces/SimilarTitlesInte
 import { ApiTvShowResponse } from '../common/interfaces/TvShowsInterfaces';
 import { TitleDetails } from '../common/interfaces/TitleDetailsInterfaces';
 
-const URL_MOVIES = process.env.REACT_APP_URL_MOVIES;
-const URL_TVSHOWS = process.env.REACT_APP_URL_TVSHOWS;
-const URL_SIMILAR_TITLES = process.env.REACT_APP_URL_SIMILAR_TITLES;
-const URL_TITLES = process.env.REACT_APP_URL_TITLES;
+// const URL_MOVIES = process.env.REACT_APP_URL_MOVIES;
+// const URL_TVSHOWS = process.env.REACT_APP_URL_TVSHOWS;
+// const URL_SIMILAR_TITLES = process.env.REACT_APP_URL_SIMILAR_TITLES;
+// const URL_TITLES = process.env.REACT_APP_URL_TITLES;
+
+const URL_BASE_MOVIEDB = process.env.REACT_APP_URL_BASE_MOVIEDB;
+const URL_TRENDING_MOVIES = `${URL_BASE_MOVIEDB}/trending/movie/week`;
+const URL_TRENDING_TVSHOWS = `${URL_BASE_MOVIEDB}/trending/tv/week`;
 const KEY = process.env.REACT_APP_KEY;
 
 
 export const api:Api = {
   movies: {
     async fetch() {
-      const url_movies_endpoint = `${URL_MOVIES}?api_key=${KEY}`;
+      const url_movies_endpoint = `${URL_TRENDING_MOVIES}?api_key=${KEY}`;
       try {
         const  {data}  = await axios.get<ApiMovieResponse>(url_movies_endpoint); 
         const movies = data.results;
@@ -27,7 +31,7 @@ export const api:Api = {
   },
   tvShows:{
     async fetch(){
-      const url_tvShows_endpoint = `${URL_TVSHOWS}?api_key=${KEY}`;
+      const url_tvShows_endpoint = `${URL_TRENDING_TVSHOWS}?api_key=${KEY}`;
       try {
         const { data } = await axios.get<ApiTvShowResponse>(url_tvShows_endpoint); 
         const tvShows = data.results;
@@ -40,7 +44,7 @@ export const api:Api = {
   details:{
     async fetch(titleId:number, mediaType:string){
 
-      const url_titleDetail_endpoint = `${URL_TITLES}/${mediaType}/${titleId}?api_key=${KEY}&language=en-US`;
+      const url_titleDetail_endpoint = `${URL_BASE_MOVIEDB}/${mediaType}/${titleId}?api_key=${KEY}&language=en-US`;
 
       try {
         const  {data}  = await axios.get<TitleDetails>(url_titleDetail_endpoint); 
@@ -52,7 +56,7 @@ export const api:Api = {
   },
   similarTitles: {
     async fetch(titleId:number, mediaType:string){
-      const url_similarTitles_endpoint = `${URL_SIMILAR_TITLES}/${mediaType}/${titleId}/similar?api_key=${KEY}`;
+      const url_similarTitles_endpoint = `${URL_BASE_MOVIEDB}/${mediaType}/${titleId}/similar?api_key=${KEY}`;
       try {
         const { data } = await axios.get<ApiSimilarTitlesResponse>(url_similarTitles_endpoint);        
         const similarTitles = data.results;
