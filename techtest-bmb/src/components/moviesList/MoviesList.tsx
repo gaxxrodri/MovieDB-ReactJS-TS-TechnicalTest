3efe2d3744edd4199b0f76e6ccf26293fd560cd1
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-07-04 19:44:03 +0200

    Add styles in MoviesList component

--- techtest-bmb/src/components/moviesList/MoviesList.tsx
+++ techtest-bmb/src/components/moviesList/MoviesList.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable react-hooks/exhaustive-deps */
+
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -14,27 +13,25 @@ const MoviesList:React.FC = ():JSX.Element => {
 
   useEffect(() => {
     dispatch(loadMovies());
-  }, []);
+  }, [dispatch]);
 
   return (
-    <div className="movieList-container">
-      {/* <h1 className="movieList-container__title">Top ranked movies from this week</h1> */}
-      <ul >
-        {movies.map(({ id, title, poster_path, vote_average }:Movie) => (
-          <div className="movieList-container__list-item">
-            <li>
-              <button key={id} type="button">
+    <section  className="movieList-container">
+      <div className="movieList-container__title">
+        <h1>Trending movies</h1>
+      </div>
+      <ul className="list" >
+        {movies.map(({ id, title, poster_path, vote_average}:Movie) => (
+            <li className="list-item" key={id}>
                 <Link to={`/movie/details/${id}`}>
-                    {title}
-                    <img src={`https://image.tmdb.org/t/p/original/${poster_path}`} alt="movie poster" height="10%" width="10%" />
-                    {vote_average}
+                    <img src={`https://image.tmdb.org/t/p/original/${poster_path}`} className="list-item__img" alt="movie poster"/>
+                    <p className="list-item__title">{title}</p>
+                    <p className="list-item__rating">{vote_average} / 10 </p>
                 </Link>
-              </button>
             </li>
-          </div>
         ))}
       </ul>
-    </div>
+    </section > 
   );
 };
 
