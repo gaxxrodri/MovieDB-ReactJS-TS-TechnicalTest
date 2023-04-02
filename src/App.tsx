4361c88e399d38831f5cc40f1e06b55edad9e20f
gaxxrodri <gaspar.unlp@gmail.com>
2023-04-02 17:44:28 +1200

    test_github_actions

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import TvShowsList from './components/TvShowsList/TvShowsList';
 import store from './redux/store';
 
 function App() {
+  
   return (
     <div className="App">
       <Provider store={store({})}>
